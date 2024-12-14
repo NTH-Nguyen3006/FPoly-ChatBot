4e532b79fe4084bd1567f6d3502904e3069878bc
Nguyen3006-IT <nthn300607@gmail.com>
2024-12-14 21:27:08 +0700

    14/12/2024

--- main.py
+++ main.py
@@ -1,33 +1,4 @@
-import requests
-from flask import Flask, render_template, request, jsonify
-from api_base import generateResponse
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET"])
-def home():
-    return render_template("home.html")
-
-@app.route("/chatbot", methods=["POST"])
-def message():
-    try:
-        # code to try
-        data = request.get_json()
-        if data:
-            usermesssage = data.get("message", None)
-            bot_response = generateResponse(user_message=usermesssage)
-
-        if bot_response.get("error"):
-            return jsonify({'message': "Server error"}), 500
-        else: 
-            bot_text = bot_response['candidates'][0]['content']['parts'][0]["text"]
-            print(bot_text)
-            return jsonify({'reply': bot_text}), 200
-    
-    except Exception as e:
-        print(e)
-        return jsonify({'message': "Server error"}), 500
-    
-    
+from app import app
+        
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
--- service/api_base.py
+++ service/api_base.py
@@ -5,11 +5,18 @@ with open("config.json", "r") as read_json:
 GENAI_KEYS: list = data.get("GenAI-Key")
 
 def generateResponse(user_message, RETRIES=len(GENAI_KEYS)) -> dict | None:
+    training_data = open(r'training-bot\trainingdata.txt', mode='r', encoding='utf8').read()
     url = f'https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={GENAI_KEYS[RETRIES-1]}'
-    print(url)
     header = {"Content-Type": "application/json"}
     data = {
         "contents": [
+            {
+                'role': "user",
+                'parts': [
+                    {"text": training_data},
+                    {'text': "Kết hợp dữ liệu cho trước như trên hãy trả lời câu hỏi sau theo kiểu tương tác tự nhiên nhất, nếu câu hỏi không khớp với dữ liệu cho trước bạn có thể tự trả lời theo những kiến thức mà bạn đã được biết, một cách chi tiết nhất."},
+                ],
+            },
             {
                 'role': "user",
                 'parts': [
